252a87339db84af3ade2be3de97e1041e76671a7
Brian Maguire <bmaguir@tcd.ie>
2015-03-03 19:53:44 +0000

    moved fb scripts to .js

--- JavaScriptFunctions.js
+++ JavaScriptFunctions.js
@@ -15,6 +15,21 @@ function hello(){
 	alert("hello world");
 }
 
+function initFb(){
+	  window.fbAsyncInit = function() {
+	FB.init({appId: '776638695748072', status: true, cookie: true,
+			 xfbml: true});
+  };
+  (function() {
+	var e = document.createElement('script');
+	e.type = 'text/javascript';
+	e.src = document.location.protocol +
+	  '//connect.facebook.net/en_US/all.js';
+	e.async = true;
+	document.getElementById('fb-root').appendChild(e);
+  }());
+}
+
 function myMousedown(e){
 	  var mouseX = e.pageX - this.offsetLeft;
 	  var mouseY = e.pageY - this.offsetTop;
