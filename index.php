29fd733f07a59ee6b7222772dbe67c279e7d017a
cuddyc <cuddyc@tcd.ie>
2015-03-12 10:48:19 +0000

    Photoselector try 2

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ width=1000 max-height=700>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="csphotoselector.js"></script>
+<script src="FurtherJSFunctions.js"></script>
 
 <script type="text/javascript">
 
