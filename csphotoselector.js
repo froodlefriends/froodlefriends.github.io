dde0e191083498daa23b3c160e241e8ce7153b82
cuddyc <cuddyc@tcd.ie>
2015-03-16 22:22:48 +0000

    Photoselector try 59

--- csphotoselector.js
+++ csphotoselector.js
@@ -378,7 +378,7 @@ var CSPhotoSelector = (function(module, $) {
 				e.preventDefault();
 				console.log('testing!!!!!!!!!!!!!!!!!');
 				console.log(this);
-				var dom = document.createElement(this.innerHTML);
+				var dom = document.createElement((string)this.innerHTML);
 				console.log(dom);
 				selectPhotos($(this));
 			});
