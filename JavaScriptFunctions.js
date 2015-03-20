c6c41a26eb169efb2a25d581ad3beaca8029a31a
Brian Maguire <bmaguir@tcd.ie>
2015-03-20 15:52:27 +0000

    changed url to source

--- JavaScriptFunctions.js
+++ JavaScriptFunctions.js
@@ -47,7 +47,7 @@ function initFb(){
 			/* handle the result */
 				console.log("got profiile picture");
 				//var parseResponse = JSON.parse(response);
-				console.log(response.data.url);
+				console.log(response);
 				
 				img.onload = function(){
 					var c = document.getElementById("myCanvas");
@@ -267,54 +267,6 @@ function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-
-//function PostImageToFacebook( authToken, filename, mimeType, imageData, message )
-//{
-//    // this is the multipart/form-data boundary we'll use
-//    var boundary = '----ThisIsTheBoundary1234567890';
-//
-//    // let's encode our image file, which is contained in the var
-//    var formData = '--' + boundary + '\r\n'
-//    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
-//    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
-//    for ( var i = 0; i < imageData.length; ++i )
-//    {
-//        formData += String.fromCharCode( imageData[ i ] & 0xff );
-//    }
-//    formData += '\r\n';
-//    formData += '--' + boundary + '\r\n';
-//    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
-//    formData += message + '\r\n'
-//    formData += '--' + boundary + '--\r\n';
-//
-//    FB.api(
-//        "/me/photos",
-//        "POST",
-//        {
-//            "source": formData
-//        },
-//        function (response) {
-//            console.log("response");
-//            console.log(response);
-//            if (response && !response.error) {
-//                console.log("response");
-//                console.log(response);
-//            }
-//        }
-//    );
-//
-//
-//
-//    //var xhr = new XMLHttpRequest();
-//    //xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
-//    //xhr.onload = xhr.onerror = function() {
-//    //    console.log( xhr.responseText );
-//		//alert("Your Picture has Been Added to FACEBOOK");
-//    //};
-//    //xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
-//    //xhr.sendAsBinary( formData );
-//}
-
 // Post a BASE64 Encoded PNG Image to facebook
 function PostImageToFacebook(authToken) {
     var canvas = document.getElementById("myCanvas");
@@ -346,7 +298,7 @@ function PostImageToFacebook(authToken) {
                         if (response && !response.error) {
                             FB.ui({
                                 method: 'share',
-                                href: response.data.url
+                                href: response.source
                             }, function(response){});
                         }
                     }
